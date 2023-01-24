d57caa7b524e556585ef8b9f74c0c04332ca3960
Jonny Porter <mailtojon@icloud.com>
2023-01-23 17:26:25 -0700

    Completing the Add Remove Tests (#1)

--- cypress/support/pom/addRemoveElementsPage.js
+++ cypress/support/pom/addRemoveElementsPage.js
@@ -1,6 +1,25 @@
-/**
- * @returns the delete button
- */
-export function getDeleteButton() {
-    return cy.contains('button', 'Delete');
+class AddRemoveElementsPage {
+
+    /**
+     * Visits the add remove elements page
+     */
+    visitPage() {
+        return cy.visit("https://the-internet.herokuapp.com/add_remove_elements/");
+    }
+
+    /**
+     * @returns the delete button
+     */
+    getDeleteButton() {
+        return cy.get('button:contains("Delete")');
+    }
+
+    /**
+     * @returns the add element button
+     */
+    getAddElementButton() {
+        return cy.contains('button', 'Add Element');
+    }
 }
+
+export default AddRemoveElementsPage;
